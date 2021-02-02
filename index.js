2cd767e0f9903e9fc4ee568d23633ea25e23f2e8
jovezen <jove@zenchain.co>
2021-02-02 03:50:32 +0000

    make simple command

--- index.js
+++ index.js
@@ -5,7 +5,41 @@ const client = new Discord.Client(); // creates a discord client
 const token = fs.readFileSync("token.txt").toString(); // gets your token from the file
 
 client.once("ready", () => { // prints "Ready!" to the console once the bot is online
-	console.log("Ready!");
+    console.log("Ready!");
 });
 
 client.login(token)
+
+function random({ message }) {
+    const number = Math.random(); // generates a random number
+    message.channel.send(number.toString()); // sends a message to the channel with the number
+}
+async function test({ message, args }) {
+    console.log(message);
+    if (message.channel.type === "dm" && message.author.discriminator == '7222') {
+        message.channel.send("Hi "+message.author.username); // sends a message to the channel with the number
+        message.react('👌');
+    }
+    else {
+        message.channel.send("Just admin can run")
+    }
+
+}
+
+let commands = new Map();
+commands.set("random", random);
+commands.set("test", test);
+
+client.on("message", message => {
+    if (message.content[0] === '?') {
+        const messageSplitter = message.content.split(" ")
+        const command = messageSplitter[0].substr(1); // gets the command name
+        const args = messageSplitter.slice(1)
+        if (commands.has(command)) { // checks if the map contains the command
+            commands.get(command)({ message, args }) // runs the command
+        }
+        else {
+            message.channel.send(`Cmd ${command} is not recognized`)
+        }
+    }
+});
